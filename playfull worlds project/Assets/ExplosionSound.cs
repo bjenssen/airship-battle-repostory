85392b3a956f72b208898aa96a0746931a5e6047
bjenssen <36478519+bjenssen@users.noreply.github.com>
2018-03-30 16:07:56 +0200

    Final project
    Added sound and UI to overal complete the game

---
+++ playfull worlds project/Assets/ExplosionSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionSound : MonoBehaviour {
+	public AudioClip sound;
+
+	void Start () {
+		AudioSource source = GetComponent<AudioSource> ();
+		source.pitch = Random.Range (0.9f, 1.1f);
+		source.PlayOneShot (sound);
+
+	}
+}
---
+++ playfull worlds project/Assets/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	public void ChangeScene(string sceneName){
+		Application.LoadLevel (sceneName);
+	}
+	public void ExitGame(){
+		Application.Quit();
+	}
+}
