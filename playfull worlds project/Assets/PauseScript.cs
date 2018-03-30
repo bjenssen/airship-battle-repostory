05ff1079644bf068c6a8f347d9931d3908629d50
bjenssen <36478519+bjenssen@users.noreply.github.com>
2018-03-30 20:10:39 +0200

    final visual update
    Extra visual update, including an Esc option.
    Game file is also included

---
+++ playfull worlds project/Assets/PauseScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+	public GameObject enemies;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)){
+			Application.LoadLevel ("Title screen");
+		}
+		if (enemies == null) {
+			Application.LoadLevel ("Title screen");
+		}
+	}
+}
